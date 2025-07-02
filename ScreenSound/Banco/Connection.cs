a5b2bbdc371ed1e910d60a76c82a7a82e66d6f6a
GersonAparecidoLima <gersonaparecido@gmail.com>
2025-07-02 12:21:42 -0300

    Conexão Banco

---
+++ ScreenSound/Banco/Connection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Banco;
+internal class Connection
+{
+    // private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+    // private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+       private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False";
+
+
+    public SqlConnection ObterConexao()
+    {
+        return new SqlConnection(connectionString);
+    }
+}
